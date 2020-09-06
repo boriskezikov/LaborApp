96bfea17f80a8de6a702bc8b33829f57c565be9c
boriskezikov <boris200898@icloud.com>
2020-09-06 23:32:39 +0300

    ExceptionInterceptor

--- src/main/java/com/force/labor/dto/Sort.java
+++ src/main/java/com/force/labor/dto/Sort.java
@@ -1,11 +1,15 @@
 package com.force.labor.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
 import javax.swing.SortOrder;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Sort {
     @NonNull
     private String sortBy;
